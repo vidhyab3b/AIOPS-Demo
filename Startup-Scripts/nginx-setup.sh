ec409b896bb414c8a9ead47802422dbca39a4a0d
vidhyab3b <vidyavece@gmail.com>
2025-10-27 16:12:40 +0800

    Update nginx-setup.sh

--- Startup-Scripts/nginx-setup.sh
+++ Startup-Scripts/nginx-setup.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@node1" bash -s <<'EOF'
+sudo yum install nginx -y
+# Change nginx listen port to 8080 (IPv4 and IPv6)
+sudo sed -i 's/listen\s\+80;/listen 8080;/' /etc/nginx/nginx.conf
+sudo sed -i 's/listen\s\+\[::\]:80;/listen [::]:8080;/' /etc/nginx/nginx.conf
+sudo systemctl start nginx; sudo systemctl enable nginx
+EOF
