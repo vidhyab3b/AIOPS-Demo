9fb31785bf1a12d3d93496ecf1c529585903e1cb
vidhyab3b <vidyavece@gmail.com>
2025-10-27 22:49:37 +0800

    Refactor mysql-db-creation.sh for better readability
    Split installation commands for clarity and added echo statement.

--- Startup-Scripts/mysql-db-creation.sh
+++ Startup-Scripts/mysql-db-creation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-3.noarch.rpm --nogpgcheck && \
-        dnf install -y mysql-shell --nogpgcheck && dnf install jq -y && \
-        dnf clean all
+
+echo "Installing mysql-shell & jq"
+dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-3.noarch.rpm --nogpgcheck
+dnf install -y mysql-shell --nogpgcheck
+dnf install jq -y
+dnf clean all
