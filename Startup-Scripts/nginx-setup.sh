9fe357790a131486f0ed032891116d16793cdf24
vidhyab3b <vidyavece@gmail.com>
2025-10-27 19:31:12 +0800

    Update nginx-setup.sh

--- Startup-Scripts/nginx-setup.sh
+++ Startup-Scripts/nginx-setup.sh
@@ -33,8 +33,8 @@ echo "Installing nginx in the $HOST"
 sudo yum install nginx -y
 
 # Change nginx listen port to 8080 (IPv4 and IPv6)
-sudo sed -i 's/listen\s\+80;/listen 8080;/' /etc/nginx/nginx.conf
-sudo sed -i 's/listen\s\+\[::\]:80;/listen [::]:8080;/' /etc/nginx/nginx.conf
+sudo sed -i 's/listen\s\+80\(\s\+default_server\)\?;/listen 8080\1;/' /etc/nginx/nginx.conf
+sudo sed -i 's/listen\s\+\[::\]:80\(\s\+default_server\)\?;/listen [::]:8080\1;/' /etc/nginx/nginx.conf
 echo "Configured nginx to listen in port 8080 in the $HOST"
 
 sudo systemctl start nginx
