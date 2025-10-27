7280e0d93beea8e4c334873d1534e840c8b7c44d
vidhyab3b <vidyavece@gmail.com>
2025-10-27 22:48:33 +0800

    Create mysql-db-creation.sh

---
+++ Startup-Scripts/mysql-db-creation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+dnf install -y https://dev.mysql.com/get/mysql80-community-release-el8-3.noarch.rpm --nogpgcheck && \
+        dnf install -y mysql-shell --nogpgcheck && dnf install jq -y && \
+        dnf clean all
