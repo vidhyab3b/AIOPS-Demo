a4138af9460edb79de0b5bb626ebcfdc700ec539
vidhyab3b <vidyavece@gmail.com>
2025-10-27 16:23:01 +0800

    Update nginx-setup.sh

--- Startup-Scripts/nginx-setup.sh
+++ Startup-Scripts/nginx-setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "root@node1" bash -s <<'EOF'
+
+# Prompt for username and password
+read -p "Enter the Bastion Node's FQDN: " HOST
+read -p "Enter the Bastion Node's Username: " USERNAME
+read -s -p "Enter the Bastion Node's Password: " PASSWORD
+echo
+
+# Run the SSH command using sshpass
+sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$USERNAME@$HOST" bash -s <<'EOF'
 sudo yum install nginx -y
+
 # Change nginx listen port to 8080 (IPv4 and IPv6)
 sudo sed -i 's/listen\s\+80;/listen 8080;/' /etc/nginx/nginx.conf
 sudo sed -i 's/listen\s\+\[::\]:80;/listen [::]:8080;/' /etc/nginx/nginx.conf
-sudo systemctl start nginx; sudo systemctl enable nginx
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
 EOF
