bbb6a15555ae3b54655fe349c0bacfef8fc82624
vidhyab3b <vidyavece@gmail.com>
2025-10-27 23:30:39 +0800

    Update aiproxy.sh

--- Startup-Scripts/aiproxy.sh
+++ Startup-Scripts/aiproxy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-oc import-image node:latest --from=docker.io/library/node:latest --confirm -n aiops
-echo "Imported the image for AIOps UI"
-oc new-app --name aiopsui https://github.com/vidhyab3b/AIOPS.git --strategy=Docker --context-dir=AIOPSUI
-oc patch bc aiopsui -p '{
+oc import-image nodejs:18 --from=registry.access.redhat.com/ubi8/nodejs-18 --confirm -n aiops
+echo "Imported the image for AIOps Proxy"
+
+oc new-app --name aiproxy https://github.com/vidhyab3b/AIOPS.git --strategy=Docker --context-dir=aiproxy
+oc patch bc aiproxy -p '{
   "spec": {
     "resources": {
       "limits": {
@@ -18,15 +19,25 @@ oc patch bc aiopsui -p '{
   }
 }'
 
-echo "Starting the build of AIOps UI after setting the resources"
-oc cancel-build aiopsui-1
-oc start-build aiopsui --follow
+echo "Starting the build of AIOps Proxy after setting the resources"
+oc cancel-build aiproxy-1
+oc start-build aiproxy --follow
 
 echo "Set the resource limits for the deployment"
-oc set resources deployment/aiopsui \
+oc set resources deployment/aiproxy \
   --limits=cpu=300m,memory=512Mi \
   --requests=cpu=250m,memory=256Mi
-oc rollout restart deployment/aiopsui
 
-oc patch svc aiopsui -n aiops --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/port", "value": 443}]'
-oc expose svc aiopsui
+oc rollout restart deployment/aiproxy
+
+oc patch svc aiproxy -n aiops --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/port", "value": 443}]'
+oc expose svc aiproxy
+
+
+
+
+
+
+
+
+
