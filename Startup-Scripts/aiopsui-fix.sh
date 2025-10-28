fa949c84f24d22967d53ac4d07b4d6c4fbc62f58
vidhyab3b <vidyavece@gmail.com>
2025-10-28 10:44:34 +0800

    Update aiopsui-fix.sh

--- Startup-Scripts/aiopsui-fix.sh
+++ Startup-Scripts/aiopsui-fix.sh
@@ -3,7 +3,7 @@
 cd /root
 read -p "Enter the GIT TOKEN of AIOPS Repo: " GIT_TOKEN
 REPO_URL="https://vidhyab3b:"$GIT_TOKEN"@github.com/vidhyab3b/AIOPS.git"
-COMMIT_MSG="Modify vite.config.js and store.js"
+COMMIT_MSG="Modify vite.config.js, store.js and application.properties"
 WORK_DIR="temp_git_repo_$$"
 
 # Clone repo
@@ -15,6 +15,7 @@ git remote set-url origin $REPO_URL
 echo; echo "Making Changes in vite.config.js"
 UI_ROUTE_URL=$(oc get route aiopsui -o jsonpath='{.spec.host}')
 sed -i "s|^\s*allowedHosts: \[.*\]|      allowedHosts: ['$UI_ROUTE_URL']|" AIOPSUI/vite.config.js
+sed -i "s|^quarkus\.http\.cors\.origins=.*|quarkus.http.cors.origins=http://localhost:8000,http://localhost,http://$UI_ROUTE_URL|" aiops_q_ms_v2/src/main/resources/application.properties
 
 echo; echo "Making Changes in store.js"
 PROXY_ROUTE_URL=$(oc get route aiproxy -o jsonpath='{.spec.host}')
@@ -35,3 +36,6 @@ cd /root; rm -rf "$WORK_DIR"
 
 echo; echo "Building the AIOps UI Deployment with the Changes"
 oc start-build aiopsui --follow
+
+echo; echo "Building the AIOps Quarkus Deployment with the Changes"
+oc start-build aiops-qks --follow
