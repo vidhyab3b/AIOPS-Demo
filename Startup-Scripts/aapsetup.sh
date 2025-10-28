95aad3ec30969f3986ad1551ec49b2600f6a227b
vidhyab3b <vidyavece@gmail.com>
2025-10-28 14:31:16 +0800

    Update aapsetup.sh

--- Startup-Scripts/aapsetup.sh
+++ Startup-Scripts/aapsetup.sh
@@ -1,1 +1,51 @@
+#!/bin/bash
 
+read -p "Enter the AAP Console URL: " CONSOLE_URL
+while [[ -z "$CONSOLE_URL" ]]; do
+    read -p "Enter the AAP Controller URL: " CONSOLE_URL
+    if [[ -z "$CONSOLE_URL" ]]; then
+        echo "AAP CONSOLE URL cannot be empty. Please try again."
+    fi
+done
+CONTROLLER_URL="$CONSOLE_URL/api/controller/v2"
+
+read -p "Enter the AAP Username: " AAP_USERNAME
+while [[ -z "$AAP_USERNAME" ]]; do
+    read -p "Enter the Bastion Node's Username: " AAP_USERNAME
+    if [[ -z "$AAP_USERNAME" ]]; then
+        echo "Username cannot be empty. Please try again."
+    fi
+done
+
+read -s -p "Enter the AAP Password: " AAP_PASSWORD
+while [[ -z "$AAP_PASSWORD" ]]; do
+    read -s -p "Enter the Bastion Node's Password: " AAP_PASSWORD
+    echo
+    if [[ -z "$AAP_PASSWORD" ]]; then
+        echo "Password cannot be empty. Please try again."
+    fi
+done
+
+# Find the Org ID of ‘Default’ & Create an Inventory ‘RHEL’
+ORG_ID=$(curl -sk -u "$USERNAME:$PASSWORD" "$CONTROLLER_URL/organizations/" | jq -r '.results[] | select(.name=="Default") | .id')
+curl -k -u lab-user:MzExNzEz \
+  -H "Content-Type: application/json" \
+  -X POST $CONTROLLER_URL/inventories/ \
+  -d '{"name": "RHEL", "organization": 1}'
+echo; echo "Created an Inventory 'RHEL'"
+
+# Find the Inventory ID of ‘RHEL’ & Create a host ‘node1’
+INVENTORY_ID=$(curl -sk -u "$USERNAME:$PASSWORD" "$CONTROLLER_URL/inventories/?name=RHEL" | jq -r '.results[0].id')
+Nginx_Server=`nslookup node1 | grep -i Name | awk '{print $2}'`
+curl -sk -u "$USERNAME:$PASSWORD" \
+  -H "Content-Type: application/json" \
+  -X POST "$CONTROLLER_URL/hosts/" \
+  -d "{\"name\": \"$Nginx_Server\", \"inventory\": $INVENTORY_ID}"
+echo; echo "Added an host to the Inventory 'RHEL'"
+
+# Find the Credential ID
+CRED_ID=$(curl -sk -u "$USERNAME:$PASSWORD" \
+  "$CONTROLLER_URL/credentials/?name=lab-credential" | \
+  jq -r '.results[0].id')
+
+echo; echo "Configured the credentials"
