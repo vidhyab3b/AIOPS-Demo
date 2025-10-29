39ae7c9119eb29a0e6be11172554bc13de53ca29
vidhyab3b <vidyavece@gmail.com>
2025-10-29 10:59:42 +0800

    Update login-setup.sh

--- Startup-Scripts/login-setup.sh
+++ Startup-Scripts/login-setup.sh
@@ -53,6 +53,32 @@ while [[ -z "$OCP_PASSWORD" ]]; do
     fi
 done
 
+read -p "Enter the AAP Console URL: " CONSOLE_URL
+while [[ -z "$CONSOLE_URL" ]]; do
+    read -p "Enter the AAP Controller URL: " CONSOLE_URL
+    if [[ -z "$CONSOLE_URL" ]]; then
+        echo "AAP CONSOLE URL cannot be empty. Please try again."
+    fi
+done
+CONTROLLER_URL="$CONSOLE_URL/api/controller/v2"
+
+read -p "Enter the AAP Username: " AAP_USERNAME
+while [[ -z "$AAP_USERNAME" ]]; do
+    read -p "Enter the Bastion Node's Username: " AAP_USERNAME
+    if [[ -z "$AAP_USERNAME" ]]; then
+        echo "Username cannot be empty. Please try again."
+    fi
+done
+
+read -s -p "Enter the AAP Password: " AAP_PASSWORD
+while [[ -z "$AAP_PASSWORD" ]]; do
+    read -s -p "Enter the AAP Password: " AAP_PASSWORD
+    echo
+    if [[ -z "$AAP_PASSWORD" ]]; then
+        echo "Password cannot be empty. Please try again."
+    fi
+done
+
 # Prompt for GIT Token
 read -p "Enter the GIT TOKEN of AIOPS Repo: " GIT_TOKEN
 
@@ -65,3 +91,8 @@ echo "export OCP_USERNAME=\"$OCP_USERNAME\"" >> /root/aiops_ocp_demojam.env
 echo "export OCP_PASSWORD=\"$OCP_PASSWORD\"" >> /root/aiops_ocp_demojam.env
 
 echo "export GIT_TOKEN=\"$GIT_TOKEN\"" >> /root/aiops_ocp_demojam.env
+
+echo "export CONSOLE_URL=\"$CONSOLE_URL\"" >> /root/aiops_ocp_demojam.env
+echo "export CONTROLLER_URL=\"$CONTROLLER_URL\"" >> /root/aiops_ocp_demojam.env
+echo "export AAP_USERNAME=\"$AAP_USERNAME\"" >> /root/aiops_ocp_demojam.env
+echo "export AAP_PASSWORD=\"$AAP_PASSWORD\"" >> /root/aiops_ocp_demojam.env
