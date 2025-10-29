eb4fcade5e5a9e60178f26f1d3bcd45657879f63
vidhyab3b <vidyavece@gmail.com>
2025-10-29 10:51:31 +0800

    Update login-setup.sh

--- Startup-Scripts/login-setup.sh
+++ Startup-Scripts/login-setup.sh
@@ -25,3 +25,38 @@ while [[ -z "$PASSWORD" ]]; do
         echo "Password cannot be empty. Please try again."
     fi
 done
+
+# Prompt for OCP API URL
+read -p "Enter the OCP API URL: " OCP_API_URL
+while [[ -z "$OCP_API_URL" ]]; do
+    read -p "Enter the OCP API URL: " OCP_API_URL
+    if [[ -z "$OCP_API_URL" ]]; then
+        echo "OCP_API_URL cannot be empty. Please try again."
+    fi
+done
+
+# Prompt for OCP Username
+read -p "Enter the OCP Username: " OCP_USERNAME
+while [[ -z "$OCP_USERNAME" ]]; do
+    read -p "Enter the OCP Username: " OCP_USERNAME
+    if [[ -z "$OCP_USERNAME" ]]; then
+        echo "USERNAME cannot be empty. Please try again."
+    fi
+done
+
+# Prompt for OCP PAssword
+read -s -p "Enter the OCP Password: " OCP_PASSWORD
+while [[ -z "$OCP_PASSWORD" ]]; do
+    read -s -p "Enter the OCP Password: " OCP_PASSWORD
+    if [[ -z "$OCP_PASSWORD" ]]; then
+        echo "PASSWORD cannot be empty. Please try again."
+    fi
+done
+
+echo "export BASTION_HOST=\"$BASTION_HOST\"" > /root/aiops_ocp_demojam.env
+echo "export USERNAME=\"$USERNAME\"" >> /root/aiops_ocp_demojam.env
+echo "export PASSWORD=\"$PASSWORD\"" >> /root/aiops_ocp_demojam.env
+
+echo "export OCP_API_URL=\"$OCP_API_URL\"" >> /root/aiops_ocp_demojam.env
+echo "export OCP_USERNAME=\"$OCP_USERNAME\"" >> /root/aiops_ocp_demojam.env
+echo "export OCP_PASSWORD=\"$OCP_PASSWORD\"" >> /root/aiops_ocp_demojam.env
