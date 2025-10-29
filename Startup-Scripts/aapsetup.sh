d079f296c2498853864efa9889d2ea36703f93be
vidhyab3b <vidyavece@gmail.com>
2025-10-29 11:00:21 +0800

    Update aapsetup.sh

--- Startup-Scripts/aapsetup.sh
+++ Startup-Scripts/aapsetup.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-read -p "Enter the AAP Console URL: " CONSOLE_URL
-while [[ -z "$CONSOLE_URL" ]]; do
-    read -p "Enter the AAP Controller URL: " CONSOLE_URL
-    if [[ -z "$CONSOLE_URL" ]]; then
-        echo "AAP CONSOLE URL cannot be empty. Please try again."
-    fi
-done
-CONTROLLER_URL="$CONSOLE_URL/api/controller/v2"
-
-read -p "Enter the AAP Username: " AAP_USERNAME
-while [[ -z "$AAP_USERNAME" ]]; do
-    read -p "Enter the Bastion Node's Username: " AAP_USERNAME
-    if [[ -z "$AAP_USERNAME" ]]; then
-        echo "Username cannot be empty. Please try again."
-    fi
-done
-
-read -s -p "Enter the AAP Password: " AAP_PASSWORD
-while [[ -z "$AAP_PASSWORD" ]]; do
-    read -s -p "Enter the AAP Password: " AAP_PASSWORD
-    echo
-    if [[ -z "$AAP_PASSWORD" ]]; then
-        echo "Password cannot be empty. Please try again."
-    fi
-done
+source /root/aiops_ocp_demojam.env
 
 # Find the Org ID of ‘Default’ & Create an Inventory ‘RHEL’
 ORG_ID=$(curl -sk -u "$AAP_USERNAME:$AAP_PASSWORD" "$CONTROLLER_URL/organizations/" | jq -r '.results[] | select(.name=="Default") | .id')
